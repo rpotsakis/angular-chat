c92718cad390e6a1c461dd33122cacb22be8c2b6
Rick Potsakis <rick.potsakis@citrix.com>
2016-07-25 14:58:36 -0700

    [added] chat details service [added] chat details (message) mock data

--- app/chat-details/chat-details.js
+++ app/chat-details/chat-details.js
@@ -10,6 +10,24 @@ angular.module('myApp.chatDetails', ['ngRoute'])
   });
 }])
 
-.controller('ChatDetailsCtrl', ['$scope','$routeParams', function($scope, $routeParams) {
+.service('chatDetailsService', function($http) {
+    this.getData = function(chatId) {
+        return $http({
+            method: 'GET',
+            url: 'mockdata/messages/'+ chatId +'.json'
+         });
+    }
+})
+
+.controller('ChatDetailsCtrl', ['$scope','$routeParams', '$location', '$mdDialog', 'chatDetailsService', function($scope, $routeParams, $location, $mdDialog, chatListService) {
   $scope.chatId = $routeParams.chatId
+  $scope.messages = [];
+
+	chatListService.getData($scope.chatId).then(function(response) {
+		$scope.messages = response.data;
+	})
+	.catch(function() {
+		$scope.error = 'unable to retrieve chat details data';
+	});
+
 }]);
