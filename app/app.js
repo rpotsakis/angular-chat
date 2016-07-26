3c2fb6b888936d1b6ae4ded22338b23e1fa9d0a6
Rick Potsakis <rick.potsakis@citrix.com>
2016-07-25 17:57:31 -0700

    [added] user factory with basic data

--- app/app.js
+++ app/app.js
@@ -11,4 +11,16 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/chat-list'});
-}]);
+}])
+.factory('userFactory', function() {
+	return {
+		getUser : function(){
+			return {
+				"id":"e7fe026d-49e8-4c21-919c-215369d269bb",
+				"firstName": "Stan",
+				"lastName": "Winston",
+				"avatar": "https://robohash.org/ametverodolore.png?size=60x60&set=set1"
+			};
+		}
+	};
+});
